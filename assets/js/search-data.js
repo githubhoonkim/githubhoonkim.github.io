c68866b8b6eefb293f6e0cdbb88b6ad96005f41d
githubhoonkim <36380497+githubhoonkim@users.noreply.github.com>
2026-04-14 04:37:52 +0000

    Deploying to gh-pages from @ githubhoonkim/githubhoonkim.github.io@1fe6b4da80dc279d364c98665070c324956a4c96 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,11 +440,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-축하-한국연구재단-개인기초연구사업-선정-및-연구원-모집",
-          title: '축하! 한국연구재단 개인기초연구사업 선정 및 연구원 모집',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_1/";
             },},{id: "news-본-연구실-minds-lab-의-연구-성과가-국제-저명-학술지인-ieee-access에-게재되었습니다-논문-제목-automated-detection-and-recognition-of-engraved-serial-numbers-on-metallic-surfaces-저자-김훈-hoon-kim-내용-금속-표면에-각인된-일련번호를-딥러닝-기반으로-자동-탐지-및-인식하는-기술을-제안하였으며-제조-지능화-및-자동-광학-검사-aoi-분야에서-높은-활용도가-기대됩니다-상세-내용은-publications-메뉴에서-확인하실-수-있습니다",
           title: '본 연구실(MINDS Lab)의 연구 성과가 국제 저명 학술지인 IEEE Access에 게재되었습니다. 논문 제목:...',
           description: "",
